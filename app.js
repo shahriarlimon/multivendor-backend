5f425703e6e46dfa36e48da7803dbfd92111c543
shahriarlimon <96817971+shahriarlimon@users.noreply.github.com>
2023-05-26 23:25:47 +0600

    changed origin local host deployed frontend server

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.json())
 /* app.use(express.urlencoded({ extended: true })); */
 app.use(
     cors({
-        origin: "http://localhost:3000",
+        origin: "https://e-shop-35f19-e7e98.web.app",
         credentials: true,
     })
 );
