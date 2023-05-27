e6ea8ba6dfb06524930c6396a8ac7781b5129c5c
shahriarlimon <96817971+shahriarlimon@users.noreply.github.com>
2023-05-28 01:22:33 +0600

    added sameSite and secure option to the cookie options to set cookie on the browser

--- utils/sendToken.js
+++ utils/sendToken.js
@@ -3,6 +3,8 @@ exports.sendToken = (user, statusCode, res) => {
     const token = user.getJwtToken();
     const options = {
         expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
+        sameSite: "none",
+        secure: true,
         httpOnly: true
     }
     res.status(statusCode).cookie("token", token, options).json({
