82878c4c8ec97b4a66927650451579429a0c7ab4
shahriarlimon <96817971+shahriarlimon@users.noreply.github.com>
2023-05-28 01:46:39 +0600

    changed that in shop-token cookie also

--- utils/shopToken.js
+++ utils/shopToken.js
@@ -2,6 +2,8 @@ exports.shopToken = (seller, statusCode, res) => {
     const token = seller.getJwtToken();
     const options = {
         expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
+        sameSite: "none",
+        secure: true,
         httpOnly: true
     }
     res.status(statusCode).cookie("seller_token", token, options).json({
